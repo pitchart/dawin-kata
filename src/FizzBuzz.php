1b2f15db087d0966e5d671f8f8ea6a4db700f833
apugnet@x2i.fr <apugnet>
2020-10-21 14:20:24 +0200

    Implemented FizzBuzz rule

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -6,20 +6,20 @@ namespace Kata;
 
 class FizzBuzz
 {
+    const FIZZ = 'Fizz';
+    const BUZZ = 'Buzz';
+
     public function of(int $number)
     {
-        if ($number % 3 == 0 && $number % 5 == 0) {
-            return 'FizzBuzz';
+        $return = '';
+        if ($number % 3 == 0) {
+            $return .= self::FIZZ;
         }
 
-        if ($number % 3 == 0) {
-            return 'Fizz';
+        if ($number % 5 == 0) {
+            $return .= self::BUZZ;
         }
-        else {
-            if ($number % 5 == 0) {
-                return 'Buzz';
-            }
-            return $number;
-        }
+
+        return $return != '' ? $return : $number;
     }
 }
