dc8b74fe1adc01372645c04c1778b3862729ddae
apugnet@x2i.fr <apugnet>
2020-10-21 14:55:33 +0200

    Implemented Fizz rule for FizzBuzz2

--- src/FizzBuzz2.php
+++ src/FizzBuzz2.php
@@ -5,5 +5,13 @@ namespace Kata;
 
 class FizzBuzz2 extends FizzBuzz
 {
+    public function of(int $number)
+    {
+        $parent = parent::of($number);
 
+        if (strstr($parent, '3') !== false) {
+            return self::FIZZ;
+        }
+        return $parent;
+    }
 }
